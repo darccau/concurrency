7c2402005512b2f8059129877b05100d71e1f576
darccau <darccau@protonmail.com>
2021-12-27 08:59:07 -0300

    feat(parallel_linked_list): add method add

--- parallel-linked-list/parallel_linked_list.cpp
+++ parallel-linked-list/parallel_linked_list.cpp
@@ -1,30 +1,75 @@
 #include <iostream>
 
-  /* add templte */
-  class Linked_list {
+  struct node {
+  int data;
+  struct node * next_node;
+};
 
-  privite:
-  int * m_head;
-  int * m_linked_list;
+/* add templte */
+class Linked_list {
+
+  private:
+  struct node * m_head;
+  struct node * m_chain;
+  /* threads do tipo B realizam buscas na list
+     threads do tipo I realizam isercao de itens no final da lista
+     threads do tipo R reliza remocao no final da lista */
+  /* struct threads */
   int m_length;
 
   public:
-  void create_node(int data);
-  void create_linked_list();
+  Linked_list();
+  struct node * create_node(int data);
   void add(int data);
-  int pop();
-  search(int target);
-  void display(int * linked_list);
+  struct node * pop();
+  bool search(int target);
+  void display();
 
 };
 
-  void Linked_list::create_node();
-  void Linked_list::create_linked_list();
-  void Linked_list::add();
-  void Linked_list::pop();
-  void Linked_list::search();
-  bool Linked_list::search(int target);
-  void Linked_list::display();
+
+struct node * Linked_list::pop() {
+  struct node * removed_node;
+
+  m_chain = m_head->next_node;
+
+  while (m_chain->next_node->next_node) {
+    m_chain = m_chain->next_node;
+  }
+
+  removed_node = m_chain->next_node;
+
+  delete m_chain->next_node;
+  m_chain->next_node = NULL;
+
+  m_length--;
+
+  return removed_node;
+}
+
+bool Linked_list::search(int target) {
+  m_chain = m_head->next_node;
+
+  while (m_chain) {
+    if (m_chain->data == target) {
+      return true;
+    }
+    m_chain = m_chain->next_node;
+  }
+
+return false;
+}
+
+void Linked_list::display() {
+  m_chain = m_head->next_node;
+
+  while (m_chain) {
+    std::cout << "[" << m_chain->data << "]" << std::endl;
+    m_chain = m_chain->next_node;
+  }
+}
+
+
 
 int main (void) {
 
