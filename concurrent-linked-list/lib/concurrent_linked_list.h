08955621e1c8d89179269c4ec064ad5dcafa0233
darccau <darccau@protonmail.com>
2022-01-16 00:24:53 -0300

    feat(concurrent_linked_list): add run method

--- concurrent-linked-list/lib/concurrent_linked_list.h
+++ concurrent-linked-list/lib/concurrent_linked_list.h
@@ -9,16 +9,25 @@
 class Concurrent_linked_list: public Linked_list {
 
   private:
+  int m_counter;
   std::mutex m_mutex;
   std::thread * m_threads_B;
   std::thread * m_threads_I;
   std::thread * m_threads_R;
 
+  int m_search_threads_number;
+  int m_insertion_threads_number;
+  int m_remotion_threads_number;
+
   public:
-  Concurrent_linked_list(int b_threads_size, int i_threads_size, int r_threads_size);
-  void concurrent_insert(int target);
-  void concurrent_search(int target);
+  Concurrent_linked_list();
+  void concurrent_insert(int data);
+  void concurrent_search(int data);
   void concurrent_remove(int postion);
+  void set_insertion_threads_number(int number);
+  void set_remotion_threads_number(int number);
+  void set_search_threads_number(int number);
+  void run();
 
 };
 
