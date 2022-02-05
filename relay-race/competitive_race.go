f54caa22c34e6e62b0e5e17c2ae8ff08626cb286
darccau <darccau@protonmail.com>
2022-02-05 02:58:33 -0300

    feat: use waitgroup to control flow execution

--- relay-race/competitive_race.go
+++ relay-race/competitive_race.go
@@ -2,40 +2,63 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func run(runnersChannel chan int, runner int) {
-	fmt.Printf("The runner %v start running\n", runner)
-	time.Sleep(1 * time.Second)
-	runner += 1
+func displayBanner() {
+	fmt.Print("###################\n")
+	fmt.Print("# The race beggin #\n")
+	fmt.Print("###################\n")
+}
 
-	if runner == 4 {
-		fmt.Print("The race is over")
-		return
-	}
+func run(runnersChannel chan int, runner int) {
+	// fmt.Printf("The runner [%v] from team [channel] start running\n", runner)
+	time.Sleep(1 * time.Millisecond)
 
 	runnersChannel <- runner
 }
 
-func main() {
-	// runnersChannel := make(chan int, 4)
+func prepareTeam(wg *sync.WaitGroup, teamName string, team [4]int, teamChannel chan int) {
+	go run(teamChannel, team[0])
+	<-teamChannel
 
-	fmt.Print("###################\n")
-	fmt.Print("# The race beggin #\n")
-	fmt.Print("###################\n")
+	go run(teamChannel, team[1])
+	<-teamChannel
+
+	go run(teamChannel, team[2])
+	<-teamChannel
+
+	go run(teamChannel, team[3])
+	<-teamChannel
+
+	fmt.Printf("Team [%v] end the race\n", teamName)
+	wg.Done()
+}
+
+func main() {
+
+	displayBanner()
+
+	var wg sync.WaitGroup
+	wg.Add(4)
+
+	brasilChannel := make(chan int)
+	chileChannel := make(chan int)
+	paraguaiChannel := make(chan int)
+	argentinaChannel := make(chan int)
 
 	teams := make(map[string][4]int)
 
 	teams["brasil"] = [4]int{1, 2, 3, 4}
 	teams["chile"] = [4]int{1, 2, 3, 4}
-	teams["Paraguai"] = [4]int{1, 2, 3, 4}
-	teams["Argentina"] = [4]int{1, 2, 3, 4}
-	fmt.Print(teams)
+	teams["paraguai"] = [4]int{1, 2, 3, 4}
+	teams["argentina"] = [4]int{1, 2, 3, 4}
 
-	// go run(runnersChannel, runner)
-	// go run(runnersChannel, <-runnersChannel)
-	// go run(runnersChannel, <-runnersChannel)
-	// go run(runnersChannel, <-runnersChannel)
+	go prepareTeam(&wg, "brasil", teams["brasil"], brasilChannel)
+	go prepareTeam(&wg, "chile", teams["chile"], chileChannel)
+	go prepareTeam(&wg, "paraguai", teams["paraguai"], paraguaiChannel)
+	go prepareTeam(&wg, "argentina", teams["argentina"], argentinaChannel)
 
+	wg.Wait()
 }
