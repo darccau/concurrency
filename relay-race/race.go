0432966ee3eb8ef8549c02bd958ca150f1ca1c78
darccau <darccau@protonmail.com>
2022-02-06 16:02:41 -0300

    feat: add some logs to race

--- relay-race/race.go
+++ relay-race/race.go
@@ -8,23 +8,32 @@ import (
 // simular corrida de revezamento
 // 4 corredores
 // o sedundo o terceiro e o quarto corredor nao podem correr ate receberem o bastao de seu atecessor
-// utilizar o metodos sleep para simular o tempo utilizado
+// utilizar o metodos sleep para simular o tempo de percorrimento do trajeto
 
-func run(runners chan int, runner int) {
-	fmt.Printf("Runner %v start run\n", runner)
+func displayBanner() {
+	fmt.Print("###################\n")
+	fmt.Print("#    Start Race   #\n")
+	fmt.Print("###################\n")
+}
+
+func run(runnersChannel chan int, runner int) {
+	fmt.Printf("The runner %v start running\n", runner)
 	time.Sleep(1 * time.Second)
 	runner += 1
-	runners <- runner
+
+	fmt.Printf("The pipe has been passed to runner[%v]\n", runner)
+	runnersChannel <- runner
 }
 
 func main() {
-	runners := make(chan int)
-	go run(runners, 1)
-	fmt.Printf("The pipe was passed\n")
-	go run(runners, <-runners)
-	fmt.Println("The pipe was passed\n")
-	go run(runners, <-runners)
-	fmt.Println("The pipe was passed\n")
-	go run(runners, <-runners)
-	fmt.Println("The race was ended\n")
+	var runner int = 1
+	runnersChannel := make(chan int)
+
+	displayBanner()
+
+	go run(runnersChannel, runner)
+	go run(runnersChannel, <-runnersChannel)
+	go run(runnersChannel, <-runnersChannel)
+	go run(runnersChannel, <-runnersChannel)
+	fmt.Print("The race was ended")
 }
